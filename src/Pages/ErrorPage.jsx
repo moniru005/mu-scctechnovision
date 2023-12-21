6da2fc6f80a2cf4a9f044d2290601017c790c9e5
Monir <moniru005@gmail.com>
2023-12-21 11:10:57 +0600

    intsall react router and create home page

---
+++ src/Pages/ErrorPage.jsx
@@ -0,0 +1,10 @@
+
+const ErrorPage = () => {
+    return (
+        <div>
+            <h2>404 Error Page</h2>
+        </div>
+    );
+};
+
+export default ErrorPage;
---
+++ src/Routes/Route.jsx
@@ -0,0 +1,18 @@
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../Layouts/MainLayout";
+import ErrorPage from "../Pages/ErrorPage";
+import Home from "../Pages/Home/Home";
+
+export const Route = createBrowserRouter([
+    {
+        path: '/',
+        element: <MainLayout></MainLayout>,
+        errorElement: <ErrorPage></ErrorPage>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>
+            }
+        ]
+    }
+])
